fc1c5f68ef8546721dfb6304cf0f7355dfb6fd27
MeDGranD <ivanred289@gmail.com>
2022-12-12 10:40:17 +0300

    Сделал 11 лабораторную работу

---
+++ lab11/TaskC.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <assert.h>
+
+typedef enum {
+    Search, //Состояние поиска разделителя
+    Separator, //Состояние проверки следующего за разделителем символа (число или нет?)
+    Number, //Состояние считывания целой части числа
+    Dot, //Состояние считывания дробной части числа
+    EndNumber //Состояние проверки на следующий за C символ (разделитель или нет) и вывод ответа
+} State;
+
+int is_number (char c){ 
+    if(('0'<=c)&&(c<='9')){ return 1; } 
+    return 0; 
+}
+
+int is_C (char c){ 
+    if(c=='C'){ return 1; } 
+    return 0; 
+}
+
+int is_separator(char c){ 
+    if((c==' ')||(c=='\n')||(c==',')||(c=='\t')){ return 1; } 
+    return 0; 
+}
+
+int is_dot(char c){ 
+    if(c=='.'){ return 1; } 
+    return 0; 
+}
+
+void test(){
+
+    assert(is_number('0'));
+    assert(is_C('C'));
+    assert(is_separator(' '));
+    assert(is_dot('.'));
+
+    assert(!is_number('C'));
+    assert(!is_C('1'));
+    assert(!is_separator('/'));
+    assert(!is_dot('g'));
+
+}
+
+int main(){
+
+    test();
+
+    double temp, dot, z; //Температура (целая часть), дробная часть, кол-во разрядов в dot
+    dot = temp = z = 0;
+    char c;
+
+    State state = Separator;
+
+    while((c=getchar())!=EOF){
+
+        switch (state){
+
+            case Search:
+                if(is_separator(c)){ state = Separator; } //Поиск разделителя
+                break;
+
+            case Separator:
+                if(is_number(c)){ //Проверка на число
+                    temp = (c - '0'); 
+                    state = Number; 
+                }
+                else { state = Search; }
+                break;
+
+            case Number:
+                if(is_number(c)){ //Считывание числа
+                    temp *= 10; 
+                    temp += (c - '0'); 
+                }
+                else if(is_C(c)){ //Окончание числа
+                    temp = temp*18/10 + 32;
+                    state = EndNumber; 
+                }
+                else if(is_dot(c)){ //Проверка на наличие дробной части
+                    temp = temp*18/10 + 32; 
+                    z = 10; dot = 0; 
+                    state = Dot; 
+                }
+                else { state = Search; }
+                break;
+
+            case EndNumber:
+                if(is_separator(c)){ //Проверка на полноценность слова
+                    printf("%g%c%c", temp, 'F', ' ');
+                    temp = 0;
+                    dot = 0;
+                    state = Separator; 
+                }
+                else { state = Search; }
+                break;
+
+            case Dot:
+                if(is_number(c)){ //Считывание дробной части
+                    z*=10;  
+                    dot *= 10; 
+                    dot += (c - '0'); 
+                }
+                else if(is_C(c)){ //Проверка на окончание слова
+                    dot = dot*18;
+                    temp += dot/z;
+                    if(z==10){ state = Search; } //Проверка на наличие дробной части
+                    else { state = EndNumber; }
+                }
+                else { state = Search; }
+                break;
+        }
+    }
+    return 0;
+}
