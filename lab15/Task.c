0177f7b5bd9b468cfd61126cd52671d8a5a98b8d
MeDGranD <ivanred289@gmail.com>
2022-12-24 00:50:35 +0300

    Добавил выбор размерности матрицы

--- lab15/Task.c
+++ lab15/Task.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <assert.h>
-#define Size 3
+#define N 10000
 
-long long mas[Size][Size]; //Матрица
-int eq[Size]; //Матрица сумм для сравнения
-int result[Size]; //Матрица вывода столбцов
 
-int Equal(int i, int j){ //Функция сравнения двух столбцов 
+long long mas[N][N]; //Матрица
+int eq[N]; //Матрица сумм для сравнения
+int result[N]; //Матрица вывода столбцов
+
+int Equal(int i, int j, long long Size){ //Функция сравнения двух столбцов 
     for(int g = 0; g < Size; g++) { if(mas[g][i]!=mas[g][j]) {return 0;}}
     return 1;
 }
 
 void test(){
 
-    assert(Equal(1,2));
+    assert(Equal(1,2,4));
     mas[0][0] = 1;
-    assert(!Equal(0,1));
+    assert(!Equal(0,1,4));
     mas[0][0] = 0;
 
 }
@@ -23,7 +24,8 @@ void test(){
 int main(){
 
     test();
-
+    long long Size;
+    scanf("%lli", &Size);
     for (int i = 0; i < Size; i++) { //Считывание матрицы
         for (int j = 0; j < Size; j++) {
             scanf("%lli", &mas[i][j]);
@@ -32,7 +34,7 @@ int main(){
     }
     for (int i = 0; i < Size; i++) { //Поиск и сравнение одинаковых столбцов матрицы
         for (int j = i+1; j < Size; j++) {
-            if (eq[i]==eq[j]) { if(Equal(i, j)){ result[j] = -1; } }
+            if (eq[i]==eq[j]) { if(Equal(i, j, Size)){ result[j] = -1; } }
         }
     }
     for(int i = 0; i < Size*Size; i++){ //Вывод конечной матрицы
